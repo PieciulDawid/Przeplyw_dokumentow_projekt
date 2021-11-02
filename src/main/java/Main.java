77544e6c8052425ca338e2e611bb2368a0eac5b7
Dawid Pieciul <pieciul.dawid@gmail.com>
2021-11-02 17:56:34 +0100

    Wstepny panel logowanie

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,205 +1,61 @@
-import com.googlecode.lanterna.*;
-import com.googlecode.lanterna.graphics.TextGraphics;
-import com.googlecode.lanterna.input.KeyStroke;
+import com.googlecode.lanterna.TerminalSize;
+import com.googlecode.lanterna.TextColor;
+import com.googlecode.lanterna.graphics.ThemedTextGraphics;
+import com.googlecode.lanterna.gui2.*;
 import com.googlecode.lanterna.screen.Screen;
 import com.googlecode.lanterna.screen.TerminalScreen;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
 import com.googlecode.lanterna.terminal.Terminal;
-import com.googlecode.lanterna.terminal.virtual.DefaultVirtualTerminal;
-import com.googlecode.lanterna.SGR;
-import com.googlecode.lanterna.input.KeyType;
 
-
-
-import javax.swing.*;
 import java.io.IOException;
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 
 public class Main {
 
     public  static void main(String[] args) throws IOException {
-        //System.out.println("hi");
-//
-        //Terminal terminal = new DefaultTerminalFactory().createTerminal();
-        //Screen screen = new TerminalScreen(terminal);
-        //TextGraphics tg = screen.newTextGraphics();
-//
-        //screen.startScreen();
-//
-        //tg.putString(10, 5, "Hello Lanterna!");
-//
-        //screen.refresh();
-        //screen.readInput();
-        //screen.stopScreen();
-//
-        ////tg.drawRectangle()
 
 
-               /*
-        In this second tutorial, we'll expand on how to use the Terminal interface to provide more advanced
-        functionality.
-        */
-        DefaultTerminalFactory defaultTerminalFactory = new DefaultTerminalFactory();
-        //TerminalSize terminal = new TerminalSize(80, 50);
-        Terminal terminal = null;
-        try {
-            terminal = defaultTerminalFactory.createTerminal();
-            /*
-            Most terminals and terminal emulators supports what's known as "private mode" which is a separate buffer for
-            the text content that does not support any scrolling. This is frequently used by text editors such as nano
-            and vi in order to give a "fullscreen" view. When exiting from private mode, the previous content is usually
-            restored, including the scrollback history. Emulators that don't support this properly might at least clear
-            the screen after exiting.
-            You can use the enterPrivateMode() to activate private mode, but you'll need to remember to also exit
-            private mode afterwards so that you don't leave the terminal in a weird state when the application exists.
-            The usual close() at the end will do this automatically, but you can also manually call exitPrivateMode()
-            and finally Lanterna will register a shutdown hook that tries to restore the terminal (including exiting
-            private mode, if necessary) as well.
-             */
-            terminal.enterPrivateMode();
+        // Setup terminal and screen layers
+        Terminal terminal = new DefaultTerminalFactory().createTerminal();
+        Screen screen = new TerminalScreen(terminal);
+        screen.startScreen();
 
-            /*
-            The terminal content should already be cleared after switching to private mode, but in case it's not, the
-            clear method should make all content set to default background color with no characters and the input cursor
-            in the top-left corner.
-             */
-            terminal.clearScreen();
+        // Create panel to hold components
+        Panel panel = new Panel();
+        panel.setLayoutManager(new GridLayout(2));
 
-            /*
-            It's possible to tell the terminal to hide the text input cursor
-             */
-            terminal.setCursorVisible(false);
+        panel.addComponent(new Label("Forename"));
+        panel.addComponent(new TextBox());
 
-            /*
-            Instead of manually writing one character at a time like we did in the previous tutorial, an easier way is
-            to use a TextGraphic object. You'll see more of this object later on, it's reused on the Screen and TextGUI
-            layers too.
-             */
-            final TextGraphics textGraphics = terminal.newTextGraphics();
+        panel.addComponent(new Label("Surname"));
+        panel.addComponent(new TextBox().setMask('*'));
 
-            /*
-            The TextGraphics object keeps its own state of the current colors, separate from the terminal. You can use
-            the foreground and background set methods to specify it and it will take effect on all operations until
-            further modified.
-             */
-            textGraphics.setForegroundColor(TextColor.ANSI.WHITE);
-            textGraphics.setBackgroundColor(TextColor.ANSI.BLUE);
-            /*
-            putString(..) exists in a couple of different flavors but it generally works by taking a string and
-            outputting it somewhere in terminal window. Notice that it doesn't take the current position of the text
-            cursor when doing this.
-             */
-            textGraphics.putString(2, 1, "Kalkulator", SGR.BOLD);
-            textGraphics.setForegroundColor(TextColor.ANSI.DEFAULT);
-            textGraphics.setBackgroundColor(TextColor.ANSI.DEFAULT);
-            //textGraphics.putString(terminal.getTerminalSize().getColumns()/2, 3, "Terminal Size: ", SGR.BOLD);
-            //textGraphics.putString(terminal.getTerminalSize().getColumns()/2 + "Terminal Size: ".length(), 3, terminal.getTerminalSize().toString());
-
-            /*
-            You still need to flush for changes to become visible
-             */
-            terminal.flush();
-
-            /*
-            You can attach a resize listener to your Terminal object, which will invoke a callback method (usually on a
-            separate thread) when it is informed of the terminal emulator window changing size. Notice that maybe not
-            all implementations supports this. The UnixTerminal, for example, relies on the WINCH signal being sent to
-            the java process, which might not make it though if you remote shell isn't forwarding the signal properly.
-             */
-            /*terminal.addResizeListener((terminal1, newSize) -> {
-                // Be careful here though, this is likely running on a separate thread. Lanterna is threadsafe in
-                // a best-effort way so while it shouldn't blow up if you call terminal methods on multiple threads,
-                // it might have unexpected behavior if you don't do any external synchronization
-                textGraphics.drawLine(5, 3, newSize.getColumns() - 1, 3, ' ');
-                textGraphics.putString(newSize.getColumns()/2, 3, "Terminal Size: ", SGR.BOLD);
-                textGraphics.putString(newSize.getColumns()/2 + "Terminal Size: ".length(), 3, newSize.toString());
+        panel.addComponent(new EmptySpace(new TerminalSize(0,0))); // Empty space underneath labels
 
 
-                try {
-                    terminal1.flush();
-                }
-                catch(IOException e) {
-                    // Not much we can do here
-                    throw new RuntimeException(e);
-                }
-            });*/
+        // Create window to hold the panel
+        BasicWindow window = new BasicWindow();
 
-            textGraphics.putString(5, 4, "Podana liczba: ", SGR.BOLD);
-            textGraphics.putString(5 + "Podana liczba: ".length(), 4, "<Wpisz Następną liczbę>");
-            textGraphics.putString(5, 5, "Lista liczb: ", SGR.BOLD);
-            textGraphics.putString(5 + "Lista liczb; ".length(), 5, "<Brak liczb>");
-            textGraphics.putString(5, 6, "Suma: ", SGR.BOLD);
-            textGraphics.putString(5 + "Suma: ".length(), 6, "<Tu będzie suma>");
-            terminal.flush();
+        window.setHints(Arrays.asList(Window.Hint.CENTERED));
+        window.setComponent(panel);
 
-
-            /*
-            Now let's try reading some input. There are two methods for this, pollInput() and readInput(). One is
-            blocking (readInput) and one isn't (pollInput), returning null if there was nothing to read.
-             */
-
-            KeyStroke keyStroke1 = terminal.readInput();
-            /*
-            The KeyStroke class has a couple of different methods for getting details on the particular input that was
-            read. Notice that some keys, like CTRL and ALT, cannot be individually distinguished as the standard input
-            stream doesn't report these as individual keys. Generally special keys are categorized with a special
-            KeyType, while regular alphanumeric and symbol keys are all under KeyType.Character. Notice that tab and
-            enter are not considered KeyType.Character but special types (KeyType.Tab and KeyType.Enter respectively)
-             */
-
-            int suma = 0;
-            List lista = new ArrayList();
-            while (keyStroke1.getKeyType() != KeyType.Escape){
-                String tekst = "";
-
-
-                KeyStroke keyStroke = terminal.readInput();
-                while(keyStroke.getKeyType() != KeyType.Enter) {
-                    textGraphics.drawLine(5, 4, terminal.getTerminalSize().getColumns() - 1, 4, ' ');
-                    textGraphics.putString(5, 4, "Podana liczba: ", SGR.BOLD);
-                    textGraphics.putString(5 + "Podana liczba: ".length(), 4, tekst += keyStroke.getCharacter().toString());
-
-                    terminal.flush();
-                    keyStroke = terminal.readInput();
-                }
-                terminal.clearScreen();
-                System.out.println(Integer.parseInt(tekst));
-                lista.add(Integer.parseInt(tekst));
-                suma += Integer.parseInt(tekst);
-
-                textGraphics.putString(2, 1, "Kalkulator", SGR.BOLD);
-                textGraphics.setForegroundColor(TextColor.ANSI.DEFAULT);
-                textGraphics.setBackgroundColor(TextColor.ANSI.DEFAULT);
-                textGraphics.putString(5, 4, "Podana liczba: ", SGR.BOLD);
-                textGraphics.putString(5 + "Podana liczba: ".length(), 4, "<Wpisz Następną liczbę>");
-                textGraphics.putString(5, 5, "Lista liczb: ", SGR.BOLD);
-                textGraphics.putString(5 + "Lista liczb: ".length(), 5, String.valueOf(lista));
-                textGraphics.putString(5, 6, "Suma: ", SGR.BOLD);
-                textGraphics.putString(5 + "Suma: ".length(), 6, String.valueOf(suma));
-                terminal.flush();
+        // Create gui and start gui
+        MultiWindowTextGUI gui = new MultiWindowTextGUI(new SeparateTextGUIThread.Factory(), screen, new DefaultWindowManager(), new WindowShadowRenderer(), new EmptySpace(TextColor.ANSI.CYAN));
+        panel.addComponent(new Button("Submit", new Runnable() {
+            @Override
+            public void run() {
+                Window wind = new BasicWindow();
+                System.out.println(Thread.currentThread().getId());
+                System.out.println((gui.getGUIThread().getThread().getId()));
+                wind.setFixedSize(new TerminalSize(10,10));
+                gui.addWindow(wind);
             }
+        }));
+        gui.addWindow(window);
+        ((AsynchronousTextGUIThread)gui.getGUIThread()).start();
 
-
-        }
-        catch(IOException e) {
-            e.printStackTrace();
-        }
-        finally {
-            if(terminal != null) {
-                try {
-                    /*
-                    The close() call here will exit private mode
-                     */
-                    terminal.close();
-                }
-                catch(IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-
+        System.out.println((gui.getGUIThread().getThread().getId()));
+        System.out.println(Thread.currentThread().getId());
 
     }
 }
