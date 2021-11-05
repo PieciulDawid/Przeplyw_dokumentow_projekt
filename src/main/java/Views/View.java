856cacfd5e1c5d19f66d387a1b6eb2c6c48ca4ba
Dawid Pieciul <pieciul.dawid@gmail.com>
2021-11-05 19:35:49 +0100

    Refaktoring kodu w klasie View

--- src/main/java/Views/View.java
+++ src/main/java/Views/View.java
@@ -10,52 +10,32 @@ public class View extends BasicWindow {
 	
 	public View(String a) {
 		super(a);
-		String className = this.getClass().getSimpleName();
-		try {
-			Controller = (Controllers.Controller) Class.
-					forName("Controllers." + className.substring(0,className.length()-4) + "Controller").
-					getDeclaredConstructor().
-					newInstance();
-		}
-		catch(InstantiationException e) {
-			e.printStackTrace();
-		}
-		catch(IllegalAccessException e) {
-			e.printStackTrace();
-		}
-		catch(InvocationTargetException e) {
-			e.printStackTrace();
-		}
-		catch(NoSuchMethodException e) {
-			e.printStackTrace();
-		}
-		catch(ClassNotFoundException e) {
-			e.printStackTrace();
-		}
+		controlersMaker();
 	}
 	public View() {
 		super();
+		controlersMaker();
+	}
+
+	private void controlersMaker() {
 		String className = this.getClass().getSimpleName();
 		try {
 			Controller = (Controllers.Controller) Class.
-					forName("Controllers." + className.substring(0,className.length()-4) + "Controller").
+					forName("Controllers." + className.substring(0, className.length() - 4) + "Controller").
 					getDeclaredConstructor().
 					newInstance();
-		}
-		catch(InstantiationException e) {
+		} catch (InstantiationException e) {
 			e.printStackTrace();
-		}
-		catch(IllegalAccessException e) {
+		} catch (IllegalAccessException e) {
 			e.printStackTrace();
-		}
-		catch(InvocationTargetException e) {
+		} catch (InvocationTargetException e) {
 			e.printStackTrace();
-		}
-		catch(NoSuchMethodException e) {
+		} catch (NoSuchMethodException e) {
 			e.printStackTrace();
-		}
-		catch(ClassNotFoundException e) {
+		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
 		}
 	}
+
+
 }
