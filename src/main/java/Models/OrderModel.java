703c5da2912e76f1972a72c2ff4bd4afc0729f29
dawiddmitruk <adres.do.korespondencji@o2.pl>
2021-11-07 15:20:28 +0100

    Dodalem pola do OrderModel

--- src/main/java/Models/OrderModel.java
+++ src/main/java/Models/OrderModel.java
@@ -1,3 +1,33 @@
 package Models;
 
-public class OrderModel {}
+import java.text.DateFormat;
+import java.util.Date;
+
+public class OrderModel implements Model{
+	private int Id;
+	private int ClientId;
+	private int EmployeeId;
+	private Date StartDate;
+	private State State;
+	
+	public Object[] toTableRow() {
+		return new Object[] {Id, ClientId, DateFormat.getDateInstance().format(StartDate), State};
+	}
+	
+	public enum State {
+		STARTED ("Rozpoczęto"),
+		INPROGRESS ("W trakcie"),
+		FINISHED ("Zakończone");
+		
+		private final String name;
+		
+		State(String name) {
+			this.name = name;
+		}
+		
+		@Override
+		public String toString() {
+			return name;
+		}
+	}
+}
