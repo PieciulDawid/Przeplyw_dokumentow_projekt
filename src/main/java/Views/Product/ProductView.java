4726cd97d5d8728f4eebca6e305bfb09e717be8a
Dawid Pieciul <pieciul.dawid@gmail.com>
2021-11-07 16:00:50 +0100

    Gradientowe tło

--- src/main/java/Views/Product/ProductView.java
+++ src/main/java/Views/Product/ProductView.java
@@ -4,6 +4,7 @@ import Controllers.Product.ProductController;
 import Views.View;
 import com.googlecode.lanterna.TerminalSize;
 import com.googlecode.lanterna.gui2.*;
+import com.googlecode.lanterna.gui2.table.Table;
 
 import java.util.Arrays;
 
@@ -11,37 +12,86 @@ import java.util.Arrays;
 public class ProductView extends View {
     public ProductView() {
         super("Zarządzanie produktami");
-        Panel panel = new Panel();
-        panel.setLayoutManager(new GridLayout(2));
-        panel.addComponent(new EmptySpace(new TerminalSize(0,1)));
-        panel.addComponent(new EmptySpace(new TerminalSize(0,1)));
+        Panel basePanel = new Panel();
+        basePanel.setLayoutManager(new GridLayout(2));
 
-        panel.addComponent(new Button("Dodaj",()->{
-            ((ProductController)Controller).AddProduct();
+        ActionListBox actionList = new ActionListBox();
+        basePanel.addComponent(actionList);
 
-        }).setPreferredSize(new TerminalSize(7,1)));
-        panel.addComponent(new EmptySpace(new TerminalSize(0,0)));
+        Panel searchAndTablePanel = new Panel();
+        searchAndTablePanel.setLayoutManager(new LinearLayout(Direction.VERTICAL));
+        searchAndTablePanel.setPreferredSize(new TerminalSize(50,25));
+        basePanel.addComponent(searchAndTablePanel);
 
-        panel.addComponent(new Button("Wyświetl"));
-        panel.addComponent(new EmptySpace(new TerminalSize(0,0)));
+        Panel searchPanel = new Panel();
+        searchPanel.setLayoutManager(new LinearLayout(Direction.HORIZONTAL));
+        searchAndTablePanel.addComponent(searchPanel
+                .withBorder(Borders.singleLine("Wyszukiwanie"))
+                .setPreferredSize(new TerminalSize(50, 3))
+        );
 
-        panel.addComponent(new Button("Modyfikuj"));
-        panel.addComponent(new EmptySpace(new TerminalSize(0,0)));
+        TextBox searchTextBox = new TextBox().setPreferredSize(new TerminalSize(40,1));
+        Button searchButton = new Button("Szukaj", () -> {return;});
 
-        panel.addComponent(new Button("Usuń").setPreferredSize(new TerminalSize(6,1)));
-        panel.addComponent(new EmptySpace(new TerminalSize(0,0)));
+        searchPanel.addComponent(searchTextBox);
+        searchPanel.addComponent(searchButton);
 
-        panel.addComponent(new Button("Dodaj wiele"));
-        panel.addComponent(new EmptySpace(new TerminalSize(0,0)));
+        Table<Object> table = new Table<Object>("Imie", "Nazwisko");
+        table.setSelectAction(() ->{
+            actionList.setEnabled(true);
+            setFocusedInteractable(actionList);
+            searchTextBox.setEnabled(false);
+            searchButton.setEnabled(false);
+            // TODO tworzenie i przekazanie modelu
+        });
 
-        panel.addComponent(new EmptySpace(new TerminalSize(0,0)));
-        panel.addComponent(new Button("Cofnij",()->{
+        actionList
+                .addItem("Akcja1", () -> {
+                    table.setEnabled(true);
+                    searchTextBox.setEnabled(true);
+                    searchButton.setEnabled(true);
+                    setFocusedInteractable(table);
+                    actionList.setEnabled(false);
+                })
+                .addItem("Akcja2", () -> {
+                    table.setEnabled(true);
+                    searchTextBox.setEnabled(true);
+                    searchButton.setEnabled(true);
+                    setFocusedInteractable(table);
+                    actionList.setEnabled(false);
+                })
+                .addItem("Akcja3", () -> {
+                    table.setEnabled(true);
+                    searchTextBox.setEnabled(true);
+                    searchButton.setEnabled(true);
+                    setFocusedInteractable(table);
+                    actionList.setEnabled(false);
+                })
+                .addItem("Akcja4", () -> {
+                    table.setEnabled(true);
+                    searchTextBox.setEnabled(true);
+                    searchButton.setEnabled(true);
+                    setFocusedInteractable(table);
+                    actionList.setEnabled(false);
+                })
+                .setPreferredSize(new TerminalSize(10,25))
+                .setEnabled(false);
+
+
+        table.getTableModel()
+                .addRow("Adrian", "Nowak")
+                .addRow("Jan", "Kowalski");
+
+        searchAndTablePanel.addComponent(table
+                .setPreferredSize(new TerminalSize(50, 20))
+                .withBorder(Borders.singleLine("Elementy"))
+        );
+
+        basePanel.addComponent(new Button("Cofnij",()->{
             Controller.GoBack();
         }));
-        panel.addComponent(new EmptySpace(new TerminalSize(0,0)));
-
 
         setHints(Arrays.asList(Window.Hint.CENTERED));
-        setComponent(panel);
+        setComponent(basePanel);
     }
 }
